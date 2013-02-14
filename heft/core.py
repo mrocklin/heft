8839ea8a5aa07608c75b7c58d3f783ebb73568f2
Matthew Rocklin <mrocklin@gmail.com>
2013-02-13 17:01:30 -0800

    first draft of complete HEFT algorithm

--- heft/core.py
+++ heft/core.py
@@ -1,4 +1,33 @@
+
+"""
+Heterogeneous Earliest Finish Time -- A static scheduling heuristic
+
+      Performance-effective and low-complexity task scheduling
+                    for heterogeneous computing
+                                by
+             Topcuoglu, Haluk; Hariri, Salim Wu, M
+     IEEE Transactions on Parallel and Distributed Systems 2002
+
+
+Cast of Characters:
+
+job - the job to be allocated
+agentstate - dict {agent: [jobs-run-on-agent-in-order]}
+jobstate - dict {job: agent-on-which-job-is-run}
+prec - dict {job: (jobs which directly precede job)}
+prec - dict {job: (jobs which directly succeed job)}
+compcost - function :: job, agent -> time to compute job on agent
+commcost - function :: job, job, agent, agent -> time to transfer results
+                       of one job needed by another between two agents
+
+[1]. http://en.wikipedia.org/wiki/Heterogeneous_Earliest_Finish_Time
+"""
+
 from functools import partial
+from collections import namedtuple
+from util import reverse_dict
+
+Event = namedtuple('Event', 'job start end')
 
 def wbar(ni, agents, compcost):
     """ Average computation cost """
@@ -22,3 +51,53 @@ def ranku(ni, agents, succ,  compcost, commcost):
         return w(ni) + max(c(ni, nj) + rank(nj) for nj in succ[ni])
     else:
         return w(ni)
+
+def agent_ready_time(agent, agentstate):
+    if not agentstate[agent]:
+        return 0
+    else:
+        return agentstate[agent][-1].end
+
+def earliest_finish_time(job, agentstate, jobstate, prec, compcost,
+        commcost, agent):
+    """ Earliest time that job can be executed on agent """
+    agent_ready = agent_ready_time(agent, agentstate)
+    if job in prec:
+        comm_ready = max(agent_ready_time(agent, agentstate)
+                   + commcost(p, job, agent, jobstate[p]) for p in prec[job])
+    else:
+        comm_ready = 0
+    return max(agent_ready, comm_ready) + compcost(job, agent)
+
+def allocate(job, agentstate, jobstate, prec, compcost, commcost):
+    """ Allocate job to the machine with earliest finish time
+
+    Operates in place
+    """
+    eft = partial(earliest_finish_time, job, agentstate, jobstate, prec,
+            compcost, commcost)
+    agent = min(agentstate.keys(), key=eft)
+    end = eft(agent)
+    start = end - compcost(job, agent)
+
+    agentstate[agent].append(Event(job, start, end))
+    jobstate[job] = agent
+
+def makespan(agentstate):
+    """ Finish time of last job """
+    return max(v[-1].end for v in agentstate.values() if v)
+
+def schedule(succ, agents, compcost, commcost):
+    rank = partial(ranku, agents=agents, succ=succ,
+                          compcost=compcost, commcost=commcost)
+    prec = reverse_dict(succ)
+
+    jobs = set(succ.keys()) | set(x for xx in succ.values() for x in xx)
+    jobs = sorted(jobs, key=rank)
+
+    agentstate = {agent: [] for agent in agents}
+    jobstate = dict()
+    for job in reversed(jobs):
+        allocate(job, agentstate, jobstate, prec, compcost, commcost)
+
+    return agentstate, jobstate
--- heft/tests/test_core.py
+++ heft/tests/test_core.py
@@ -1,4 +1,5 @@
-from core import wbar, cbar, ranku
+from heft.core import (wbar, cbar, ranku, schedule, Event, earliest_finish_time,
+        makespan)
 from functools import partial
 
 def compcost(job, agent):
@@ -8,6 +9,8 @@ def compcost(job, agent):
         return job * 2
 
 def commcost(ni, nj, A, B):
+    if A == B:
+        return 0
     if A.islower() == B.islower():
         return 5
     else:
@@ -34,3 +37,25 @@ def test_ranku():
     assert rank(6) == w(6)
     assert rank(4) == w(4) + c(4, 6) + rank(6)
     assert rank(2) > rank(4) > rank(3) > rank(5)
+
+def test_earliest_finish_time():
+    agentstate = {'a': [Event(2, 0, 3)], 'b': []}
+    jobstate = {2: 'a'}
+    prec = {3: (2,)}
+    assert earliest_finish_time(3, agentstate, jobstate, prec, compcost,
+        commcost, 'a') == 6
+    assert earliest_finish_time(3, agentstate, jobstate, prec, compcost,
+        commcost, 'b') == 3 + 5
+
+def test_schedule():
+    agentstate, jobstate = schedule(dag, 'abc', compcost, commcost)
+    a = jobstate[2]
+    b = jobstate[6]
+    c = jobstate[3]
+    assert agentstate == {a: [Event(2, 0, 2), Event(4, 2, 6), Event(5, 11, 16)],
+                          b: [Event(6, 5, 11)],
+                          c: [Event(3, 5, 8)]}
+
+def test_makespan():
+    assert makespan({'a': [Event(0, 0, 1), Event(1, 2, 3)],
+                     'b': [Event(2, 3, 4)]}) == 4
